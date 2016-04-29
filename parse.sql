025db461266e1cbf262c8e7f24602bc0ed1cd66c
nicki6 <nicolai.tesela@gmail.com>
2016-04-29 14:28:45 -0500

    biaj variants

--- parse.sql
+++ parse.sql
@@ -27,6 +27,7 @@ select json_populate_recordset(null::card, json_array_elements(data->'SOI'->'car
 "types"
 }
 
+--Full
 with jsn as (
     select json_array_elements(data->'SOI'->'cards') as card
     from public.all 
@@ -63,5 +64,37 @@ card->>'layout' as layout,
 card->>'number' as "number",
 card->>'rarity' as rarity,
 card->>'artist' as artist
+into public.standard
+from jsn;
+
+--abbreviated
+with jsn as (
+    select json_array_elements(data->'SOI'->'cards') as card
+    from public.all 
+    UNION ALL
+    select json_array_elements(data->'OGW'->'cards') as card
+    from public.all 
+    UNION ALL
+    select json_array_elements(data->'DTK'->'cards') as card
+    from public.all 
+    UNION ALL
+    select json_array_elements(data->'BFZ'->'cards') as card
+    from public.all 
+    UNION ALL
+    select json_array_elements(data->'ORI'->'cards') as card
+    from public.all 
+) select 
+card->>'imageName' as name,
+card->>'manaCost' as manaCost,
+card->>'text' as text,
+card->>'power' as power,
+card->>'toughness' as toughness,
+card->>'colorIdentity' as colors,
+card->>'cmc' as cmc,
+card->>'type' as "type",
+card->>'types' as types,
+card->>'subtypes' as subtypes,
+card->>'supertypes' as supertypes,
+card->>'names' as names
 into public.standard
 from jsn;
