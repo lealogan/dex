04e9d1a1d71c08afa6bc0cb8e82fd27521f8e026
nicki6 <nicolai.tesela@gmail.com>
2017-01-16 20:05:21 -0600

    finishing up deck describe

--- deck_describe.sh
+++ deck_describe.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 while read c; do
-    cardname=$(echo -n $c | tr '[:upper:]' '[:lower:]' | gsed -re "s/^[0-9 \t]+//" -e "s/\b([a-z])/\U\1/g");
-    echo $cardname;
-    jq --arg card "$cardname" \
-        '.[$card]."manaCost",
-        .[$card].type,
-        .[$card].text,
-        .[$card].power,
-        .[$card].toughness
-        ' allcards.json |\
+    cleanedcard=$(echo -n $c | tr '[:upper:]' '[:lower:]' | gsed -re "s/^[0-9 \t]+//"); # -e "s/\b([a-z])/\U\1/g");
+    #cleanedcard=$(echo -n $c | tr '[:upper:]' '[:lower:]' | gsed -re "s/^[0-9 \t]+//" -e "s/\b([a-z])/\U\1/g");
+    echo $cleanedcard;
+    jq --arg cardname "$cleanedcard" \
+        '.[] | select(.imageName == $cardname) as $card |
+        $card.manaCost,
+        $card.type,
+        $card.text,
+        $card.power,
+        $card.toughness' ~/code/dex/allcards.json |\
     gsed -e "s/null//" | cat -s;
 done < $1
