54d3a704d820eae2df99526c0d85d42a6fecbd59
Nikki Six <nicolai.tesela@gmail.com>
2018-04-06 22:55:51 -0500

    gsed -> sed

--- mtg_describe.sh
+++ mtg_describe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #jq --arg card $1 '. [] | .[$card]' temp.json
 c=$@
-cardname=$(echo $c | tr '[:upper:]' '[:lower:]' | gsed -re "s/\b([a-z])/\U\1/g")
+cardname=$(echo $c | tr '[:upper:]' '[:lower:]' | sed -re "s/\b([a-z])/\U\1/g")
 jq --arg card "$cardname" \
     '.[$card]."manaCost",
     .[$card].type,
@@ -9,4 +9,4 @@ jq --arg card "$cardname" \
     .[$card].power,
     .[$card].toughness
     ' allcards.json |\
-gsed -e "s/null//" | cat -s
+sed -e "s/null//" | cat -s
